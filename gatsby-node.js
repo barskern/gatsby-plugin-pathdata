51c410373ae8402aa7979fc84eebc3fa8e5a16e9
Ole Martin Ruud <barskern@outlook.com>
2018-11-30 20:49:38 +0100

    Replace deprecated API

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,17 @@
 
 exports.onCreateNode = (
-  { boundActionCreators, node },
+  { actions, node },
   options
 ) => {
   const { type } = node.internal
 
   if (type !== (options.matchNodeType)) return
 
-  const { createNodeField } = boundActionCreators
-
   options.extract.forEach(extractor => {
     const result = node.fileAbsolutePath.replace(extractor.selector, extractor.replacer)
 
     if (result !== null && result !== node.fileAbsolutePath) {
-      createNodeField({
+      actions.createNodeField({
         node,
         name: extractor.name,
         value: result
